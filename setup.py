e66e088e5ce952c6bb9a05c384ed00f31d9b298c
Tal Yalon <tal.yalon@supertenant.com>
2024-01-31 00:19:18 +0000

    Improve .pth install heuristics
    Since we can't tell where exactly the package will get installed when
    there are Debian installations where the system python is using
    dist-packages and pip uses site-packages, we try to install in
    any purelib directory that exists.
    To offset installing in directories that may not contain the package
    itself afterwads (and that can lead to ImportError and an ugly console
    message when the site module executes the .pth file) we also check
    in the .pth file if the package exists.
    Note that this won't work for frozen packages though.
    
    Bump version to v1.1.10.

--- setup.py
+++ setup.py
@@ -3,21 +3,29 @@ import os
 from setuptools import setup
 
 # In newer debian-based systems, there's a dist-packages directory
-# where user packages get installed (and not site-packages), so we
-# want to write our .pth file there as well.
+# where packages may get installed, depending on how the user installs the package -
+# for system python (which can be python used by the container) packages are installed
+# under dist-packages and for user packages they get installed under site-packages.
+# All in all, very confusing and no clear way to tell which one is used.
+# Since installing in the wrong place may mean we won't autoload, we want to install
+# anywhere we think is relevant.
+# In the .pth file we have a safety mechanism to check that the supermeter-autowrapt module
+# can be imported - this mechanism won't work for frozen packages though.
 def get_lib_dirs():
-    dirs = []
+    import sysconfig
+    dirs = set()
+    for scheme in sysconfig.get_scheme_names():
+        purelib = sysconfig.get_path('purelib', scheme=scheme)
+        if os.path.exists(purelib):
+            datalib = sysconfig.get_path('data', scheme=scheme)
+            if datalib and purelib.startswith(datalib):
+                dirs.add(purelib[len(datalib) + 1:])
     try:
         from distutils.sysconfig import get_python_lib
-        dirs.append(get_python_lib(prefix=''))
+        dirs.add(get_python_lib(prefix=''))
     except ImportError:
         pass
-    import sysconfig
-    purelib = sysconfig.get_paths().get('purelib', '')
-    datalib = sysconfig.get_paths().get('data', '')
-    if purelib and datalib and purelib.startswith(datalib):
-        dirs.append(purelib[len(datalib) + 1:])
-    return set(dirs)
+    return dirs
 
 
 def load_readme():
@@ -27,7 +35,7 @@ def load_readme():
 
 setup_kwargs = dict(
     name = 'supermeter-autowrapt',
-    version = '1.1.9',
+    version = '1.1.10',
     description = 'Boostrap mechanism for monkey patches for supermeter.',
     author = 'SuperTenant Ltd.',
     author_email = 'info@supertenant.com',
